fa14463ea89259e1d7520be88fa865eff9ee1f45
Giovane Ribeiro <gar018@hotmail.com>
2016-08-03 22:08:22 -0300

    File PYGR changed.

--- 03-should-we-try-something-fancier.py
+++ 03-should-we-try-something-fancier.py
@@ -4,4 +4,26 @@ import commands
 
 print ("PYGR03 - Should we try something fancier!\n")
 
-def
+def givePermissionForTheNeedy():
+    files = commands.getoutput("ls -l").split("\n")
+    del files[0]  # First line, not useful
+
+    filesChanged = [];
+    for file in files:
+        if (file[9] != 'x'):
+            data = file.split(" ")
+            fileName = data[len(data) - 1]
+            if (fileName[len(fileName) - 3:len(fileName)] == '.py'):
+                os.system("chmod +x " + fileName)
+                filesChanged.append(fileName)
+
+    return filesChanged;
+
+filesChanged = givePermissionForTheNeedy();
+
+if (len(filesChanged) > 0):
+    print("Checkout a list of changed files:")
+    for file in filesChanged:
+        print(file)
+else:
+    print("Well... there's no file to change.")
