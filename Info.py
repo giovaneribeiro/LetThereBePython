7b9580c790dba2480ca7be9a27a938afc3ee9cdd
Giovane Ribeiro <gar018@hotmail.com>
2016-08-08 21:45:19 -0300

    Having fun with PS AUX

---
+++ Info.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+
+class Info:
+    user = "DEATH"
+    number = 1
+    cpu = 0.0
+    mem = 0.0
---
+++ Process.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+class Process:
+    user = "USER"
+    pid = 0
+    cpu = 0
+    mem = 0
+    vsz = 0
+    rss = 0
+    tty = "TYY"
+    stat = "STAT"
+    start = "0:00"
+    time = "0:00"
+    command = "COMMAND"
+
